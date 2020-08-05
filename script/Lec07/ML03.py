84fec392c7f9c520d46424e99241e2bad787ee8c
Hong <39299265+hsk1321@users.noreply.github.com>
2020-08-05 20:22:07 +0900

    study

---
+++ script/Lec07/ML03.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+import numpy as np
+
+
+data = np.array([
+    [73.,  80.,  75., 152.],
+    [93.,  88.,  93., 185.],
+    [89.,  91.,  90., 180.],
+    [96.,  98., 100., 196.],
+    [73.,  66.,  70., 142.]
+], dtype=np.float32)
+
+X = data[:, :-1]
+y = data[:, [-1]]
+
+W = tf.Variable(tf.random.normal((3, 1)))
+b = tf.Variable(tf.random.normal((1,)))
+
+learning_rate = 0.000001
+
+
+def predict(X):
+    return tf.matmul(X, W) + b
+
+
+print("epoch | cost")
+
+n_epochs = 2000
+for i in range(n_epochs+1):
+    with tf.GradientTape() as tape:
+        cost = tf.reduce_mean((tf.square(predict(X) - y)))
+
+    W_grad, b_grad = tape.gradient(cost, [W, b])
+
+    W.assign_sub(learning_rate * W_grad)
+    b.assign_sub(learning_rate * b_grad)
+
+    if i % 100 == 0:
+        print("{:5} | {:10.4f}".format(i, cost.numpy()))
